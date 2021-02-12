1ff751fef09f6e83d573622f7efe6b76281b09c7
sanderdebr <sander606@gmail.com>
2021-02-12 12:06:26 +0100

    collision detection

--- src/Ball.js
+++ src/Ball.js
@@ -5,21 +5,58 @@ export default class Ball {
     this.id = id;
     this.image = new Image();
     this.image.src = WhiteBall;
-    this.x = x;
-    this.y = y;
-    this.width = 40;
-    this.height = 40;
-    this.updateX = 0.01;
-    this.updateY = 0.01;
+    this.posX = x;
+    this.posY = y;
+    this.radius = 80;
+    this.isMoving = false;
+    this.updateX = 0;
+    this.updateY = 0;
+    this.speed = 1;
+  }
+
+  move(speed, angle) {
+    this.isMoving = speed > 0;
+    this.speed = speed;
+    this.updateX = this.speed * Math.cos((angle * Math.PI) / 180);
+    this.updateY = this.speed * Math.sin((angle * Math.PI) / 180);
+  }
+
+  detectCollision(balls) {
+    for (let i = 0; i < balls.length; i++) {
+      const dx = parseInt(this.posX) - parseInt(balls[i].posX);
+      const dy = parseInt(this.posY) - parseInt(balls[i].posY);
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      // Collision detected
+      if (distance < this.radius / 4 + balls[i].radius / 4) {
+        // Find angle of collision
+        const angle =
+          (Math.atan2(this.posY - balls[i].posY, this.posX - balls[i].posX) *
+            180) /
+          Math.PI;
+
+        // If a ball is still, move it slower
+        if (!this.isMoving) {
+          this.move(balls[i].speed / 2, angle);
+        } else {
+          this.move(this.speed / 1.5, angle);
+        }
+      }
+    }
   }
 
   update(timestamp) {
-    console.log(this.updateX);
-    this.x += this.updateX;
-    this.y += this.updateY;
+    this.posX += this.updateX;
+    this.posY += this.updateY;
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.posX,
+      this.posY,
+      this.radius * 0.5,
+      this.radius * 0.5
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import "./styles.css";
 
 import Ball from "./Ball";
-import BallFactory from "./BallFactory";
 import Canvas from "./Canvas";
 
 class Game {
   constructor() {
     this.canvas = new Canvas();
-    this.entities = [
-      new Ball(1, this.canvas.width / 2 - 200, this.canvas.height / 2 - 40),
+    this.balls = [
+      new Ball(1, this.canvas.width / 2 - 200, this.canvas.height / 2 - 30),
       new Ball(2, this.canvas.width / 2, this.canvas.height / 2 - 40),
     ];
 
@@ -17,17 +16,22 @@ class Game {
   }
 
   shootBall() {
-    const targetBall = this.entities.filter((ball) => ball.id === 1)[0];
-    targetBall.updateX = 1;
+    this.balls[0].move(0.5, 0);
+    this.balls[1].move(1, -180);
   }
 
   gameLoop(timestamp) {
     const ctx = this.canvas.getContext("2d");
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    for (let i = 0; i < this.entities.length; i++) {
-      this.entities[i].update(timestamp);
-      this.entities[i].draw(ctx);
+    for (let i = 0; i < this.balls.length; i++) {
+      const otherBalls = this.balls.filter(
+        (ball) => ball.id !== this.balls[i].id
+      );
+
+      this.balls[i].detectCollision(otherBalls);
+      this.balls[i].update(timestamp);
+      this.balls[i].draw(ctx);
     }
 
     window.requestAnimationFrame(this.gameLoop.bind(this));
